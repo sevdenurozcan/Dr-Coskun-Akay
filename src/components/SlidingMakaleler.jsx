e0acf7dd23d3cbc92ca42c7f75ca76685c366fe2
sevdenurozcan <sevdenurozcan0@gmail.com>
2025-03-01 13:47:09 +0300

    Add files via upload

--- src/components/SlidingMakaleler.jsx
+++ src/components/SlidingMakaleler.jsx
@@ -12,6 +12,13 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 import img1 from "../images/glutatyon.jpg";
+import img2 from "../images/kan.jpg";
+import img3 from "../images/kanser.jpg";
+import img4 from "../images/romatizma.jpg";
+import img5 from "../images/vitamind.jpg";
+import img6 from "../images/diyabet.jpg";
+
+
 
 function SlidingMakaleler() {
     return (
@@ -36,27 +43,27 @@ function SlidingMakaleler() {
                         <p>Glutatyon vücudumuzdaki hücrelerde yapılan ve detoks safhalarının iki fazında da rol oynayan en güçlü antioksidanlardan birisidir.</p>
                     </SwiperSlide>
                     <SwiperSlide className='mySlides'>
-                        <img src="../src/images/kan.jpg" alt="Ozon güvenilir bir yöntem midir?" />
+                        <img src={img2} alt="Ozon güvenilir bir yöntem midir?" />
                         <h4>Ozon Güvenilir Bir Yöntem Midir?</h4>
                         <p>Ozon tedavisinin yapılmaması gereken bazı durumlar haricinde ozon terapi uzman hekim kontrolünde yapıldığında yan etki olasılığı çok düşük ve etkili bir tedavi yöntemidir.</p>
                     </SwiperSlide>
                     <SwiperSlide className='mySlides'>
-                        <img src="../src/images/kanser.jpg" alt="Kanser ve ozon tedavisi" />
+                        <img src={img3} alt="Kanser ve ozon tedavisi" />
                         <h4>Kanser ve Ozon Tedavisi</h4>
                         <p>Kanser toplumların korkulu rüyası olmaya devam eden, tedavisi oldukça zahmetli ve uzun süreli bir hastalıktır.</p>
                     </SwiperSlide>
                     <SwiperSlide className='mySlides'>
-                        <img src="../src/images/romatizma.jpg" alt="Romatizma ve ozon tedavisi" />
+                        <img src={img4} alt="Romatizma ve ozon tedavisi" />
                         <h4>Romatizma ve Ozon Tedavisi</h4>
                         <p>Halk arasında romatizma olarak bilinen hastalıklar 150’den fazla hastalığın ortak adıdır.</p>
                     </SwiperSlide>
                     <SwiperSlide className='mySlides'>
-                        <img src="../src/images/vitamind.jpg" alt="D vitamini ve hastalıklarla ilişkisi" />
+                        <img src={img5} alt="D vitamini ve hastalıklarla ilişkisi" />
                         <h4>D Vitamini ve Hastalıklarla İlişkisi</h4>
                         <p>D vitamini bir hormon öncü molekülüdür ve iki formda bulunur. Ergokalsiferol (vitamin D2) bitkilerde ve bazı balıklarda bulunur. </p>
                     </SwiperSlide>
                     <SwiperSlide className='mySlides'>
-                        <img src="../src/images/diyabet.jpg" alt="Ozon ile diyabet tedavisi" />
+                        <img src={img6} alt="Ozon ile diyabet tedavisi" />
                         <h4>Diyabette Ozon Tedavisi</h4>
                         <p>Diyabet, yani şeker hastalığı, pankreastan yetersiz düzeyde insulin hormonu salgılanması ya da hücrelerin insulin hormonuna karşı gösterdiği direnç sonucu ortaya çıkar.</p>
                     </SwiperSlide>
